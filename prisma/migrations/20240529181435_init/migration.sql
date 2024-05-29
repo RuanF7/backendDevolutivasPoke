5233756ad17e700e41ea568e4e1c40477af29119
RuanF7 <ruan.correia@kukac.com.br>
2024-05-29 15:18:57 -0300

    BUILD: Inicial commit

---
+++ prisma/migrations/20240529181435_init/migration.sql
@@ -0,0 +1,116 @@
+-- CreateTable
+CREATE TABLE "Pessoa" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "senha" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Aluno" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    CONSTRAINT "Aluno_id_fkey" FOREIGN KEY ("id") REFERENCES "Pessoa" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Professor" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "tipo" TEXT NOT NULL,
+    CONSTRAINT "Professor_id_fkey" FOREIGN KEY ("id") REFERENCES "Pessoa" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Curso" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "tipo" TEXT NOT NULL,
+    CONSTRAINT "Curso_tipo_fkey" FOREIGN KEY ("tipo") REFERENCES "Professor" ("tipo") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Mochila" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "professorId" INTEGER NOT NULL,
+    CONSTRAINT "Mochila_professorId_fkey" FOREIGN KEY ("professorId") REFERENCES "Professor" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Pokemon" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT NOT NULL,
+    "tipo" TEXT NOT NULL,
+    "mochilaId" INTEGER NOT NULL,
+    CONSTRAINT "Pokemon_mochilaId_fkey" FOREIGN KEY ("mochilaId") REFERENCES "Mochila" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Prova" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "cursoId" INTEGER NOT NULL,
+    "professorId" INTEGER NOT NULL,
+    "dataCriacao" DATETIME NOT NULL,
+    CONSTRAINT "Prova_cursoId_fkey" FOREIGN KEY ("cursoId") REFERENCES "Curso" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Prova_professorId_fkey" FOREIGN KEY ("professorId") REFERENCES "Professor" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Questao" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "provaId" INTEGER NOT NULL,
+    "pokemonNome" TEXT NOT NULL,
+    "perguntaTipo" TEXT NOT NULL,
+    "perguntaGolpe" TEXT NOT NULL,
+    CONSTRAINT "Questao_provaId_fkey" FOREIGN KEY ("provaId") REFERENCES "Prova" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Resposta" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "questaoId" INTEGER NOT NULL,
+    "alunoId" INTEGER NOT NULL,
+    "respostaTipo" TEXT NOT NULL,
+    "respostaGolpe" TEXT NOT NULL,
+    CONSTRAINT "Resposta_questaoId_fkey" FOREIGN KEY ("questaoId") REFERENCES "Questao" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Resposta_alunoId_fkey" FOREIGN KEY ("alunoId") REFERENCES "Aluno" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Nota" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "provaId" INTEGER NOT NULL,
+    "questaoId" INTEGER NOT NULL,
+    "alunoId" INTEGER NOT NULL,
+    "nota" REAL NOT NULL,
+    CONSTRAINT "Nota_provaId_fkey" FOREIGN KEY ("provaId") REFERENCES "Prova" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Nota_questaoId_fkey" FOREIGN KEY ("questaoId") REFERENCES "Questao" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Nota_alunoId_fkey" FOREIGN KEY ("alunoId") REFERENCES "Aluno" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Devolutiva" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "provaId" INTEGER NOT NULL,
+    "alunoId" INTEGER NOT NULL,
+    "notaFinal" REAL NOT NULL,
+    "dataDevolutiva" DATETIME NOT NULL,
+    CONSTRAINT "Devolutiva_provaId_fkey" FOREIGN KEY ("provaId") REFERENCES "Prova" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Devolutiva_alunoId_fkey" FOREIGN KEY ("alunoId") REFERENCES "Aluno" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Matricula" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "cursoId" INTEGER NOT NULL,
+    "alunoId" INTEGER NOT NULL,
+    CONSTRAINT "Matricula_cursoId_fkey" FOREIGN KEY ("cursoId") REFERENCES "Curso" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Matricula_alunoId_fkey" FOREIGN KEY ("alunoId") REFERENCES "Aluno" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Pessoa_email_key" ON "Pessoa"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Professor_tipo_key" ON "Professor"("tipo");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Mochila_professorId_key" ON "Mochila"("professorId");
