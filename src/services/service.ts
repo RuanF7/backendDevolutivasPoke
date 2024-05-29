4bb4c65b138dd9b832f5444ba80e3cde5219006d
RuanF7 <ruan.correia@kukac.com.br>
2024-05-29 15:47:12 -0300

    REFACTOR: Getting only name and move from pokeAPI

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,6 +1,21 @@
 import axios from 'axios';
 
-export const fetchPokemon = async (name: string) => {
-  const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-  return response.data;
+interface PokemonData {
+  name: string;
+  move: string;
+}
+
+export const fetchPokemon = async (name: string): Promise<PokemonData> => {
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const moves = response.data.moves;
+    const move = moves.length > 0 ? moves[0].move.name : 'No moves found';
+    const pokemonData: PokemonData = {
+      name: response.data.name,
+      move: move
+    };
+    return pokemonData;
+  } catch (error) {
+    throw new Error('Failed to fetch Pokemon data');
+  }
 };
