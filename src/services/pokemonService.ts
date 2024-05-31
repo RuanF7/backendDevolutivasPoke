f14a9b3cf7e2c673ec7c1eea763b6a05e5cda7af
RuanF7 <ruan.correia@kukac.com.br>
2024-05-31 18:40:36 -0300

    REFACTOR: Rename service.ts to pokemonService.ts and update property names in fetchPokemon function

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -2,20 +2,19 @@ import axios from 'axios';
 
 interface PokemonData {
   name: string;
-  move: string;
+  tipo: string;
 }
 
 export const fetchPokemon = async (name: string): Promise<PokemonData> => {
   try {
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    const moves = response.data.moves;
-    const move = moves.length > 0 ? moves[0].move.name : 'No moves found';
+    const types = response.data.types.map((typeEntry: any) => typeEntry.type.name);
     const pokemonData: PokemonData = {
       name: response.data.name,
-      move: move
+      tipo: types[0]  // Supondo que estamos interessados no primeiro tipo do Pokémon
     };
     return pokemonData;
   } catch (error) {
-    throw new Error('Failed to fetch Pokemon data');
+    throw new Error('Failed to fetch Pokémon data');
   }
 };
