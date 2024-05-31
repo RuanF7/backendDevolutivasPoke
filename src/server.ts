29f47dd143d4b5b7491500ba2469df0e802499c3
RuanF7 <ruan.correia@kukac.com.br>
2024-05-31 16:41:04 -0300

    FEAT: Add professorRoutes for handling professor-related API endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes';
+import professorRoutes from './routes/professorRoutes';
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/professor', professorRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
