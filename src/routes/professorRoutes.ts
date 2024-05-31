ad7fc69f1bf48253b76f35825f7ccaaf275f9282
RuanF7 <ruan.correia@kukac.com.br>
2024-05-31 18:39:54 -0300

    REFACTOR: professorRoutes to use static method for adding Pokémon to mochila

--- src/routes/professorRoutes.ts
+++ src/routes/professorRoutes.ts
@@ -2,9 +2,7 @@ import { Router } from 'express';
 import { ProfessorController } from '../controllers/professorController';
 
 const router = Router();
-const professorController = new ProfessorController();
 
-router.post('/add-pokemon', (req, res) => professorController.addPokemon(req, res));
-router.post('/create-prova', (req, res) => professorController.createProva(req, res));
+router.post('/add-pokemon', ProfessorController.addPokemonToMochila);
 
 export default router;
