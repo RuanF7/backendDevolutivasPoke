580dd788b2862b4031119eddb8f56071515d2fbd
RuanF7 <ruan.correia@kukac.com.br>
2024-05-31 10:45:08 -0300

    REFACTOR: Adding auth into server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
-import app from './app';
+import express from 'express';
+import authRoutes from './routes/authRoutes';
 
-const PORT = process.env.PORT || 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
