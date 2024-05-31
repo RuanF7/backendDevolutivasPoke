720b1b8f800ab58b4d1d1d90338dfbccb4c2b4dc
RuanF7 <ruan.correia@kukac.com.br>
2024-05-31 16:44:56 -0300

    REFACTOR: Remove unnecessary comment in userService.ts

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 const prisma = new PrismaClient();
-const JWT_SECRET = process.env.JWT_SECRET as string; // Certifique-se de que o JWT_SECRET está definido
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 interface UserInput {
   nome: string;
