565e48e35b1cec74fdad1522fd1944d234871c87
RuanF7 <ruan.correia@kukac.com.br>
2024-06-03 14:16:51 -0300

    FEAT: Add API endpoint to create prova

--- src/routes/professorRoutes.ts
+++ src/routes/professorRoutes.ts
@@ -4,5 +4,6 @@ import { ProfessorController } from '../controllers/professorController';
 const router = Router();
 
 router.post('/add-pokemon', ProfessorController.addPokemonToMochila);
+router.post('/create-prova', ProfessorController.createProva);
 
 export default router;
