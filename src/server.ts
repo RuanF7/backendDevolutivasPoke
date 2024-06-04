fb1992ec66853bdeb1db03acab9620b6e97b4ae5
RuanF7 <ruanfinamor@gmail.com>
2024-06-04 15:22:30 -0300

    FEAT: Add alunoRoutes for handling student-related API endpoints

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import authRoutes from './routes/authRoutes';
 import professorRoutes from './routes/professorRoutes';
 import provaRoutes from './routes/provaRoutes';
 import cursoRoutes from './routes/cursoRoutes';
+import alunoRoutes from './routes/alunoRoutes';  // importando as rotas dos alunos
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,6 +13,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/professor', professorRoutes);
 app.use('/api/prova', provaRoutes);
 app.use('/api/cursos', cursoRoutes);
+app.use('/api/aluno', alunoRoutes);  // adicionando o uso das rotas dos alunos
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
