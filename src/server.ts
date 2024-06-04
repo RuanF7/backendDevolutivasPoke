88ce7babd094a1bbc470c3699c0b697f260cfd69
RuanF7 <ruan.correia@kukac.com.br>
2024-06-04 09:14:28 -0300

    FEAT: Importing and using cursoRoutes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import authRoutes from './routes/authRoutes';
 import professorRoutes from './routes/professorRoutes';
 import provaRoutes from './routes/provaRoutes';
+import cursoRoutes from './routes/cursoRoutes';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/professor', professorRoutes);
 app.use('/api/prova', provaRoutes);
+app.use('/api/cursos', cursoRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
