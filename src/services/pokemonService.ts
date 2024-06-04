1b814d6460a7881b828edd46c13efe20d78f8c91
RuanF7 <ruanfinamor@gmail.com>
2024-06-04 11:09:24 -0300

    FEAT: Update fetchPokemon function to include image and moves data

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -3,15 +3,19 @@ import axios from 'axios';
 interface PokemonData {
   name: string;
   tipo: string;
+  imagem: string;
+  moves: string[];
 }
 
 export const fetchPokemon = async (name: string): Promise<PokemonData> => {
   try {
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
     const types = response.data.types.map((typeEntry: any) => typeEntry.type.name);
     const pokemonData: PokemonData = {
       name: response.data.name,
-      tipo: types[0]
+      tipo: types[0],
+      imagem: response.data.sprites.front_default,
+      moves: response.data.moves.map((move: any) => move.move.name)
     };
     return pokemonData;
   } catch (error) {
