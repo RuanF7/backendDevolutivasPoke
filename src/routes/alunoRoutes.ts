ec81dc8c855021dc3741377b868021932947db2f
RuanF7 <ruanfinamor@gmail.com>
2024-06-05 09:47:08 -0300

    FEAT: Add 'criar-devolutiva' endpoint to alunoRoutes

--- src/routes/alunoRoutes.ts
+++ src/routes/alunoRoutes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { AlunoController } from '../controllers/alunoController';
+import { ProvaController } from '../controllers/provaController';
 
 const router = express.Router();
 const alunoController = new AlunoController();
+const provaController = new ProvaController();
 
 router.post('/matricular', (req, res) => alunoController.matricularAluno(req, res));
 router.post('/realizar-prova', (req, res) => alunoController.realizarProva(req, res));
+router.post('/criar-devolutiva', (req, res) => provaController.criarDevolutiva(req, res));
 
 export default router;
