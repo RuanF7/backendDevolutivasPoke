2b2a2c3b3c1eb882d40974b4e3c4b978e40c5493
RuanF7 <ruanfinamor@gmail.com>
2024-06-07 08:46:57 -0300

    REFACTOR: Update fetchPokemon method in pokemonService.ts

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -9,13 +9,12 @@ interface PokemonData {
 
 export const fetchPokemon = async (name: string): Promise<PokemonData> => {
   try {
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
-    const types = response.data.types.map((typeEntry: any) => typeEntry.type.name);
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);    
     const pokemonData: PokemonData = {
       name: response.data.name,
-      tipo: types[0],
+      tipo: response.data.types[0].type.name,
       imagem: response.data.sprites.front_default,
-      moves: response.data.moves.map((move: any) => move.move.name)
+      moves: response.data.moves.map((move: {name: string}) => move.name)
     };
     return pokemonData;
   } catch (error) {
