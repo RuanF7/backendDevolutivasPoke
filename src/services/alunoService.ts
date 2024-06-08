ee20306be707e2347472c61382f4ff9305656019
RuanF7 <ruanfinamor@gmail.com>
2024-06-08 14:57:05 -0300

    FEAT: Add matricularAluno method to AlunoService

--- src/services/alunoService.ts
+++ src/services/alunoService.ts
@@ -1,21 +1,45 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export class AlunoService {
-  async matricularAluno(alunoId: number, cursoId: number) {
+  async matricularAluno(alunoId: number, cursoNome: string) {
+    const aluno = await prisma.aluno.findUnique({
+      where: { id: alunoId },
+    });
+
+    if (!aluno) {
+      throw new Error("Aluno não encontrado");
+    }
+
+    const curso = await prisma.curso.findFirst({
+      where: { nome: cursoNome },
+    });
+
+    if (!curso) {
+      throw new Error("Curso não encontrado");
+    }
+
     const matricula = await prisma.matricula.create({
       data: {
         alunoId,
-        cursoId,
+        cursoId: curso.id,
       },
     });
     return matricula;
   }
 
-  async realizarProva(alunoId: number, provaId: number, respostas: { questaoId: number, respostaNome: string, respostaGolpe: string }[]) {
+  async realizarProva(
+    alunoId: number,
+    provaId: number,
+    respostas: {
+      questaoId: number;
+      respostaNome: string;
+      respostaGolpe: string;
+    }[]
+  ) {
     const respostasCriadas = await prisma.resposta.createMany({
-      data: respostas.map(resposta => ({
+      data: respostas.map((resposta) => ({
         alunoId,
         questaoId: resposta.questaoId,
         respostaNome: resposta.respostaNome,
