020594d9374297e01cfca245028d3f4b6f3d8359
RuanF7 <ruanfinamor@gmail.com>
2024-06-08 14:56:31 -0300

    REFACTOR: Update alunoController.ts to use consistent import formatting and improve code readability

--- src/controllers/alunoController.ts
+++ src/controllers/alunoController.ts
@@ -1,17 +1,34 @@
-import { Request, Response } from 'express';
-import { AlunoService } from '../services/alunoService';
+import { Request, Response } from "express";
+import { AlunoService } from "../services/alunoService";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 const alunoService = new AlunoService();
 
 export class AlunoController {
   async matricularAluno(req: Request, res: Response) {
-    const { alunoId, cursoId } = req.body;
+    const { alunoId, cursoNome } = req.body;
 
+    console.log("Dados recebidos para matricular:", { alunoId, cursoNome });
     try {
-      const matricula = await alunoService.matricularAluno(alunoId, cursoId);
+      const curso = await prisma.curso.findFirst({
+        where: { nome: cursoNome },
+      });
+
+      if (!curso) {
+        return res.status(404).json({ error: "Curso não encontrado" });
+      }
+
+      const matricula = await alunoService.matricularAluno(alunoId, curso.nome);
       res.status(201).json(matricula);
     } catch (error) {
-      res.status(500).json({ error: 'Falha ao matricular aluno' });
+      if (
+        error === "Aluno não encontrado" ||
+        error === "Curso não encontrado"
+      ) {
+        return res.status(404).json({ error: error });
+      }
+      res.status(500).json({ error: "Falha ao matricular aluno" });
     }
   }
 
@@ -19,10 +36,14 @@ export class AlunoController {
     const { alunoId, provaId, respostas } = req.body;
 
     try {
-      const respostasCriadas = await alunoService.realizarProva(alunoId, provaId, respostas);
+      const respostasCriadas = await alunoService.realizarProva(
+        alunoId,
+        provaId,
+        respostas
+      );
       res.status(201).json(respostasCriadas);
     } catch (error) {
-      res.status(500).json({ error: 'Falha ao realizar a prova' });
+      res.status(500).json({ error: "Falha ao realizar a prova" });
     }
   }
 }
