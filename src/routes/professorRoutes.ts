913e9472f10f1a509b0824a8e65c12c5bc9e64d8
RuanF7 <ruanfinamor@gmail.com>
2024-06-11 17:23:37 -0300

    REFACTOR: Commenting unused routes

--- src/routes/professorRoutes.ts
+++ src/routes/professorRoutes.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-import { ProfessorController } from '../controllers/professorController';
+import { Router } from "express";
+import { ProfessorController } from "../controllers/professorController";
 
 const router = Router();
 
-router.post('/add-pokemon', ProfessorController.addPokemonToMochila);
-router.post('/corrigir-prova', ProfessorController.corrigirProva);
+router.post("/add-pokemon/", ProfessorController.addPokemon);
+// router.post('/corrigir-prova', ProfessorController.corrigirProva);
 
 export default router;
