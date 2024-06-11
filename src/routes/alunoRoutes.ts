91e56f8d58aa1cbdc5bfef97dbce4fee6779fde5
RuanF7 <ruanfinamor@gmail.com>
2024-06-11 17:21:40 -0300

    REFACTOR: Commenting unused routes

--- src/routes/alunoRoutes.ts
+++ src/routes/alunoRoutes.ts
@@ -1,13 +1,19 @@
-import express from 'express';
-import { AlunoController } from '../controllers/alunoController';
-import { ProvaController } from '../controllers/provaController';
+import express from "express";
+import { StudentController } from "../controllers/alunoController";
+// import { ProvaController } from "../controllers/provaController";
 
 const router = express.Router();
-const alunoController = new AlunoController();
-const provaController = new ProvaController();
+const alunoController = new StudentController();
+// const provaController = new ProvaController();
 
-router.post('/matricular', (req, res) => alunoController.matricularAluno(req, res));
-router.post('/realizar-prova', (req, res) => alunoController.realizarProva(req, res));
-router.post('/criar-devolutiva', (req, res) => provaController.criarDevolutiva(req, res));
+router.post("/matricular", (req, res) =>
+  StudentController.registerStudent(req, res)
+);
+// router.post("/realizar-prova", (req, res) =>
+//   alunoController.realizarProva(req, res)
+// );
+// router.post("/criar-devolutiva", (req, res) =>
+//   provaController.criarDevolutiva(req, res)
+// );
 
 export default router;
