95ccc9c3ee7096231f8c1419e124301793944108
RuanF7 <ruanfinamor@gmail.com>
2024-06-11 17:17:26 -0300

    REFACTOR: Commenting provaRoutes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes';
-import professorRoutes from './routes/professorRoutes';
-import provaRoutes from './routes/provaRoutes';
-import cursoRoutes from './routes/cursoRoutes';
-import alunoRoutes from './routes/alunoRoutes';  // importando as rotas dos alunos
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes";
+import professorRoutes from "./routes/professorRoutes";
+// import provaRoutes from './routes/provaRoutes';
+import cursoRoutes from "./routes/cursoRoutes";
+import alunoRoutes from "./routes/alunoRoutes"; // importando as rotas dos alunos
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,11 +12,11 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/professor', professorRoutes);
-app.use('/api/prova', provaRoutes);
-app.use('/api/cursos', cursoRoutes);
-app.use('/api/aluno', alunoRoutes);  
+app.use("/api/auth", authRoutes);
+app.use("/api/professor", professorRoutes);
+// app.use('/api/prova', provaRoutes);
+app.use("/api/cursos", cursoRoutes);
+app.use("/api/aluno", alunoRoutes);
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
