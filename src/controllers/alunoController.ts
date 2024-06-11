f3ef0e52f4ddf292b39279a3909305627a96f589
RuanF7 <ruanfinamor@gmail.com>
2024-06-11 17:18:50 -0300

    REFACTOR: Change in registerStudent to adapt it in new DB

--- src/controllers/alunoController.ts
+++ src/controllers/alunoController.ts
@@ -1,49 +1,65 @@
 import { Request, Response } from "express";
-import { AlunoService } from "../services/alunoService";
+import { StudentService } from "../services/alunoService";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const alunoService = new AlunoService();
+const studentService = new StudentService(prisma);
 
-export class AlunoController {
-  async matricularAluno(req: Request, res: Response) {
-    const { alunoId, cursoNome } = req.body;
+export class StudentController {
+  static async registerStudent(req: Request, res: Response) {
+    const { alunoId, cursoId } = req.body;
 
-    console.log("Dados recebidos para matricular:", { alunoId, cursoNome });
     try {
-      const curso = await prisma.curso.findFirst({
-        where: { nome: cursoNome },
+      const matricula = await studentService.registerStudent({
+        alunoId,
+        cursoId,
       });
-
-      if (!curso) {
-        return res.status(404).json({ error: "Curso não encontrado" });
-      }
-
-      const matricula = await alunoService.matricularAluno(alunoId, curso.nome);
       res.status(201).json(matricula);
     } catch (error) {
-      if (
-        error === "Aluno não encontrado" ||
-        error === "Curso não encontrado"
-      ) {
-        return res.status(404).json({ error: error });
-      }
       res.status(500).json({ error: "Falha ao matricular aluno" });
     }
   }
-
-  async realizarProva(req: Request, res: Response) {
-    const { alunoId, provaId, respostas } = req.body;
-
-    try {
-      const respostasCriadas = await alunoService.realizarProva(
-        alunoId,
-        provaId,
-        respostas
-      );
-      res.status(201).json(respostasCriadas);
-    } catch (error) {
-      res.status(500).json({ error: "Falha ao realizar a prova" });
-    }
-  }
 }
+
+// export class AlunoController {
+//   async matricularAluno(req: Request, res: Response) {
+//     const { alunoId, cursoNome } = req.body;
+
+//     console.log("Dados recebidos para matricular:", { alunoId, cursoNome });
+//     try {
+//       const curso = await prisma.curso.findFirst({
+//         where: { nome: cursoNome },
+//       });
+
+//       if (!curso) {
+//         return res.status(404).json({ error: "Curso não encontrado" });
+//       }
+
+//       const matricula = await alunoService.matricularAluno(alunoId, curso.nome);
+//       res.status(201).json(matricula);
+//     } catch (error) {
+//       if (
+//         error === "Aluno não encontrado" ||
+//         error === "Curso não encontrado"
+//       ) {
+//         return res.status(404).json({ error: error });
+//       }
+//       res.status(500).json({ error: "Falha ao matricular aluno" });
+//     }
+//   }
+
+//   async realizarProva(req: Request, res: Response) {
+//     const { alunoId, provaId, respostas } = req.body;
+
+//     try {
+//       const respostasCriadas = await alunoService.realizarProva(
+//         alunoId,
+//         provaId,
+//         respostas
+//       );
+//       res.status(201).json(respostasCriadas);
+//     } catch (error) {
+//       res.status(500).json({ error: "Falha ao realizar a prova" });
+//     }
+//   }
+// }
