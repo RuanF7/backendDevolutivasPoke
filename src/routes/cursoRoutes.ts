9f248c2926146804257f3d7dcc74e6583603f05c
RuanF7 <ruanfinamor@gmail.com>
2024-06-11 17:22:32 -0300

    REFACTOR: Commenting unused routes

--- src/routes/cursoRoutes.ts
+++ src/routes/cursoRoutes.ts
@@ -1,10 +1,13 @@
-import express, { Request, Response } from 'express';
-import { CursoController } from '../controllers/cursoController';
+import express, { Request, Response } from "express";
+import { CourseController } from "../controllers/cursoController";
 
 const router = express.Router();
-const cursoController = new CursoController();
 
-router.post('/curso', (req: Request, res: Response) => cursoController.criarCurso(req, res));
-router.get('/curso', (req: Request, res: Response) => cursoController.listarCursos(req, res));
+router.post("/curso", (req: Request, res: Response) =>
+  CourseController.createCourse(req, res)
+);
+// router.get("/curso", (req: Request, res: Response) =>
+//   courseController.listarCursos(req, res)
+// );
 
 export default router;
