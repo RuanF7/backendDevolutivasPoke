354c90ae9613bee25f289f312010a3c7d241fafa
RuanF7 <ruanfinamor@gmail.com>
2024-06-12 17:29:39 -0300

    FEAT: router to student answer test

--- src/student/routes/studentRouter.ts
+++ src/student/routes/studentRouter.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { RegisterStudentController } from "../controllers/registerStudentToCourse/registerStudentToCourse";
+import { StudentTestAnswerController } from "../controllers/studentTestAnswers/studentTestAnswers";
 const router = Router();
 
 router.post("/matricular", (req, res) =>
   RegisterStudentController.registerStudent(req, res)
+);
+router.post("/responder-prova", (req, res) =>
+  StudentTestAnswerController.createAnswer(req, res)
 );
 // router.post("/realizar-prova", (req, res) =>
 //   alunoController.realizarProva(req, res)
