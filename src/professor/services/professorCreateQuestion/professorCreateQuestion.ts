9561ee1773dd0ac302823a6ccaeaaa7b8125fd0a
RuanF7 <ruanfinamor@gmail.com>
2024-06-19 16:09:05 -0300

    FEAT: Check if professor has Pokemon before creating question

--- src/professor/services/professorCreateQuestion/professorCreateQuestion.ts
+++ src/professor/services/professorCreateQuestion/professorCreateQuestion.ts
@@ -1,11 +1,25 @@
-import { Questao, PrismaClient } from "@prisma/client";
-import { Question, QuestionResponse } from "../../../types/professorTypes";
+import { Questao, PrismaClient, Pokemon } from "@prisma/client";
+import {
+  Question,
+  QuestionResponse,
+  HavePokemon,
+} from "../../../types/professorTypes";
 
 export class ProfessorCreateQuestionService {
   private prisma: PrismaClient;
 
   constructor(prisma: PrismaClient) {
     this.prisma = prisma;
+  }
+  async professorHasPokemon(pokemon: HavePokemon): Promise<boolean> {
+    const professorPokemons = await this.prisma.pokemon.findFirst({
+      where: { professorId: pokemon.professorId, nome: pokemon.nome },
+    });
+
+    if (professorPokemons) {
+      return false;
+    }
+    return true;
   }
   async createQuestion(question: Question): Promise<Questao> {
     const questionData = await this.prisma.questao.create({
