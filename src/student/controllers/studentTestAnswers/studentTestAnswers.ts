f47691f9e7eebb5240b2490ed555a4b0784a171a
RuanF7 <ruanfinamor@gmail.com>
2024-06-19 16:10:08 -0300

    FEAT: get tests for student method

--- src/student/controllers/studentTestAnswers/studentTestAnswers.ts
+++ src/student/controllers/studentTestAnswers/studentTestAnswers.ts
@@ -8,6 +8,62 @@ import { NotFoundError } from "../../../errors/notFoundError";
 const studentTestAnswersService = new StudentTestAnswersService(prisma);
 
 export class StudentTestAnswerController {
+  static async getAvailableTests(req: Request, res: Response) {
+    const { alunoId } = req.params;
+
+    try {
+      const studentId = parseInt(alunoId);
+
+      // Verifica se o aluno existe
+      const aluno = await prisma.pessoa.findUnique({
+        where: {
+          id: studentId,
+          tipo: "aluno",
+        },
+      });
+
+      if (!aluno) {
+        throw new NotFoundError("Aluno não encontrado");
+      }
+
+      // Busca os cursos e provas disponíveis para o aluno
+      const cursosComProvas =
+        await studentTestAnswersService.getAvailableTestsByStudentId(studentId);
+
+      res.status(200).json(cursosComProvas);
+    } catch (error) {
+      console.error("Erro ao buscar provas disponíveis:", error);
+
+      if (error instanceof NotFoundError) {
+        res.status(404).json({ message: error.message });
+      } else {
+        const internalError = new InternalServerError("Erro interno.");
+        res.status(500).json({ message: internalError.message });
+      }
+    }
+  }
+
+  static async getQuestoes(req: Request, res: Response) {
+    const { provaId } = req.params;
+
+    try {
+      const questoes = await prisma.questao.findMany({
+        where: {
+          provaId: parseInt(provaId),
+        },
+      });
+
+      if (!questoes || questoes.length === 0) {
+        throw new NotFoundError("Questões não encontradas para esta prova.");
+      }
+
+      res.status(200).json(questoes);
+    } catch (error) {
+      console.error("Erro ao buscar questões:", error);
+      res.status(500).json({ message: "Erro ao buscar questões." });
+    }
+  }
+
   static async createAnswer(req: Request, res: Response) {
     console.log("Requisição:", req.body);
     const { alunoId, questaoId, resposta, nota } = req.body;
